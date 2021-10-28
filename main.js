826a91656bee65182d24d2aeea114fb7938380b8
nailtonsimoes <nailtonsimoes@gmail.com>
2021-10-28 16:10:19 -0300

    primeira versão carregada para o github

---
+++ main.js
@@ -0,0 +1,19 @@
+/*abre e fecha menu ao clicar no icone*/
+
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/*qando clicar em algum item do menu, esconder o menu*/
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
